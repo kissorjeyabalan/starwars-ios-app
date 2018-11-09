6f7c8434bc6afb74bf46fada3e2b4c37330bb7a1
Kissor Jeyabalan <git@kij.no>
2018-11-09 14:54:58 +0100

    Added convenience functions

---
+++ StarWars/Extensions/URLSession+Swapi.swift
@@ -0,0 +1,34 @@
+//
+//  URLSession+Swapi.swift
+//  StarWars
+//
+//  Created by Kissor Jeyabalan on 09/11/2018.
+//  Copyright © 2018 Kissor Jeyabalan. All rights reserved.
+//
+
+import Foundation
+
+extension URLSession {
+    fileprivate func getSwapi<T: Codable>(with url: URL, completionHandler: @escaping (T?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
+        return self.dataTask(with: url) { data, response, error in
+            guard let data = data, error == nil else {
+                completionHandler(nil, response, error)
+                return
+            }
+            
+            completionHandler(try? JSONDecoder().decode(T.self, from: data), response, nil)
+        }
+    }
+    
+    func getAllMovies(completionHandler: @escaping(SwapiResult<Movie>?, URLResponse?, Error?) -> Void) {
+        self.getSwapi(with: URL(string: "https://swapi.co/api/films/")!, completionHandler: completionHandler).resume()
+    }
+    
+    func getMovie(with url: URL, completionHandler: @escaping(Movie?, URLResponse?, Error?) -> Void) {
+        self.getSwapi(with: url, completionHandler: completionHandler).resume()
+    }
+    
+    func getMovie(with id: Int, completionHandler: @escaping(Movie?, URLResponse?, Error?) -> Void) {
+        self.getMovie(with: URL(string: "https://swapi.co/api/films/\(id)")!, completionHandler: completionHandler)
+    }
+}
